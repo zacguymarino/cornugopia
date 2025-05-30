5a2ee45967180dcf57f5acef57b62a87d29ca6a4
zacguymarino <zacguymarino@gmail.com>
2025-05-30 13:08:14 -0400

    Adjust db setup for Fly

--- app/db.py
+++ app/db.py
@@ -10,5 +10,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
 
+DATABASE_URL = DATABASE_URL.split("?", 1)[0]
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
